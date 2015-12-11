a845ef8a415a4eef074d7463c23ada63346acbe1
Maitane Azkue <maitane07@gmail.com>
2015-12-11 14:17:45 +0100

    css egiten

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+	session_start();
+	mysql_connect("localhost","root","") or die(mysql_error());
+	mysql_select_db("gureargazkiak") or die(mysql_error());
+	/*mysql_connect("mysql.hostinger.es","u517629783_mazk","123456") or die(mysql_error());
+	mysql_select_db("u517629783_garg") or die(mysql_error());*/
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -27,7 +35,7 @@
 		<script src="./js/index.js"></script>
 		<!--<script src="./js/administratzailea.js"></script>-->
 		
-		<nav class="navbar navbar-inverse navbar-fixed-top">
+		<nav class="navbar navbar-default navbar-fixed-top">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -59,7 +67,7 @@
 		
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-sm-3 col-md-2 sidebar">
+				<!--<div class="col-sm-3 col-md-2 sidebar">
 					<ul class="nav nav-sidebar">
 						<li class="active"><a href="#">1.1- <span class="sr-only">(current)</span></a></li>
 						<li><a href="#">1.2-</a></li>
@@ -76,56 +84,54 @@
 						<li><a href="">3.2-</a></li>
 						<li><a href="">3.3-</a></li>
 					</ul>
-				</div>
-				<div  class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-					<div id="datuak">
-					</div>
-					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-						<!-- Indicators -->
-						<ol class="carousel-indicators">
-							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-							<li data-target="#myCarousel" data-slide-to="1"></li>
-							<li data-target="#myCarousel" data-slide-to="2"></li>
-						</ol>
-						
-						<!-- Wrapper for slides -->
-						<div class="carousel-inner" role="listbox">
-							<div class="item active">
-								<img src="ny.jpg" alt="New York" width="1200" height="700">
-								<div class="carousel-caption">
-									<h3>New York</h3>
-									<p>The atmosphere in New York is lorem ipsum.</p>
-								</div>      
+				</div>-->
+				<!--<div  class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main"style="box-shadow: 0px 0px 5px blue">-->
+					<div id="datuak" style="box-shadow: 0px 0px 5px red">
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#myCarousel" data-slide-to="1"></li>
+								<li data-target="#myCarousel" data-slide-to="2"></li>
+							</ol>
+							
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<?php
+									$sql=mysql_query("select * from argazkia");
+									$argazkia=mysql_fetch_array($sql);
+									echo'<div class="item active">';
+									echo '<img src="data:image;base64,'.base64_encode($argazkia['Argazkia']).'" alt="'.$argazkia['Izenburua'].'" width="100">';
+									echo '<div class="carousel-caption">';
+									echo '<h3>'.$argazkia['Izenburua'].'</h3>';
+									echo '</div>';      
+									echo '</div>';
+									while($argazkia=mysql_fetch_array($sql)){
+										echo $argazkia['Izenburua'];
+										echo'<div class="item">';
+										echo '<img src="data:image;base64,'.base64_encode($argazkia['Argazkia']).'" alt="'.$argazkia['Izenburua'].'" width="100">';
+										echo '<div class="carousel-caption">';
+										echo '<h3>'.$argazkia['Izenburua'].'</h3>';
+										echo '</div>';      
+										echo '</div>';
+									}
+									
+								?>
 							</div>
 							
-							<div class="item">
-								<img src="chicago.jpg" alt="Chicago" width="1200" height="700">
-								<div class="carousel-caption">
-									<h3>Chicago</h3>
-									<p>Thank you, Chicago - A night we won't forget.</p>
-								</div>      
-							</div>
-							
-							<div class="item">
-								<img src="la.jpg" alt="Los Angeles" width="1200" height="700">
-								<div class="carousel-caption">
-									<h3>LA</h3>
-									<p>Even though the traffic was a mess, we had the best time playing at Venice Beach!</p>
-								</div>      
-							</div>
+							<!-- Left and right controls -->
+							<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
 						</div>
-						
-						<!-- Left and right controls -->
-						<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a>
 					</div>
-				</div>
+					
+				<!--</div>-->
 			</div>
 		</div>
 		
